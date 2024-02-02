5871f26b9951fb9260298c38500af0dd545fb81b
MilosJovic23 <milos.mj992@gmail.com>
2024-02-02 13:39:10 +0100

    * video params

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 
 
 import "./style.css"
-import AllVideos from "./Components/AllVideos";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Home from "./Pages/Home";
+import Video from "./Components/Video";
 
 const App= ()=>{
     return(
         <>
             <BrowserRouter>
                 <Routes>
-                    <Route path="/" element={<AllVideos/>} />
-                </Routes>
+                    <Route path="/" element={<Home />}/>
+                    <Route path="/video/:id" element={<Video />} />
+                    </Routes>
             </BrowserRouter>
             {/*// <AllVideos/>*/}
 
---
+++ src/Pages/Home.js
@@ -0,0 +1,13 @@
+
+import AllVideos from "./../Components/AllVideos";
+
+const Home=()=>{
+
+    return(
+        <>
+            <h3>Glavna Stranica</h3>
+           <AllVideos/>
+        </> )
+}
+
+export default Home;
