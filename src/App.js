a25e812097efa2d595016c2636b1c7f86f2c54e0
MilosJovic23 <milos.mj992@gmail.com>
2024-02-02 12:09:56 +0100

    * first commit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,21 @@
 
 
 import "./style.css"
-const VIDEOS=[
-    {
-        id:1,
-        title:"Ho to learn React",
-        url:"https://www.youtube.com/watch?v=SqcY0GlETPk",
-        cover:"https://i.ytimg.com/vi/SqcY0GlETPk/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&amp;rs=AOn4CLBxxEfavbxbAns6HTvgSrFA1b0Rrw",
-    },
-    {
-        id:2,
-        title:"Ho to learn css",
-        url:"https://www.youtube.com/watch?v=1PnVor36_40",
-        cover:"https://i.ytimg.com/vi/1PnVor36_40/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC4tJ7kbGq-1Vv0pz8nIkiwF5xyQg",
-    }
-]
+import AllVideos from "./Components/AllVideos";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
 const App= ()=>{
     return(
         <>
-            {VIDEOS.map((video)=> {
-                return <a href={video.url} target="_blank">
-                    <img src={video.cover}/>
-                    <h4>{video.title}</h4>
-                </a>
-            })}
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<AllVideos/>} />
+                </Routes>
+            </BrowserRouter>
+            {/*// <AllVideos/>*/}
 
 
         </>
-
 
     )
 }
---
+++ src/Components/AllVideos.js
@@ -0,0 +1,21 @@
+
+import VIDEOS from "./../videos.json"
+const getAllVideos= ()=>{
+
+
+    return(
+
+            VIDEOS.map((video)=> {
+                return <a href={video.url} target="_blank">
+                    <img src={video.cover} alt="videos"/>
+                    <h4>{video.title}</h4>
+                    </a>
+            })
+
+
+
+
+    )
+}
+
+export default getAllVideos
