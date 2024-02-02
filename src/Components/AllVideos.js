0bf0c87643279c81b05ab7137ee2ee50bd29c10c
MilosJovic23 <milos.mj992@gmail.com>
2024-02-02 12:14:22 +0100

    * changes

--- src/Components/AllVideos.js
+++ src/Components/AllVideos.js
@@ -6,7 +6,7 @@ const getAllVideos= ()=>{
     return(
 
             VIDEOS.map((video)=> {
-                return <a href={video.url} target="_blank" rel="nofollow">
+                return <a href={video.url} target="_blank">
                     <img src={video.cover} alt="videos"/>
                     <h4>{video.title}</h4>
                     </a>
